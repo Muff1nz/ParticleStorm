ec32e6264737cb4b958561777e0a8e3ab8fcc587
Muff1nz <muffinz314@gmail.com>
2019-03-29 18:50:31 +0100

    Stats class improvement

--- ParticleStorm/PhysicsEngine.cpp
+++ ParticleStorm/PhysicsEngine.cpp
@@ -147,14 +147,14 @@ void PhysicsEngine::LeadThreadRun() {
 		timer.Restart();
 		HandleExplosions();
 		timer.Stop();
-		environment->stats.puEventsTotalLastSecond += timer.ElapsedMilliseconds();
+		environment->stats.puEventsTotalLastSecond += timer.ElapsedMicroseconds();
 
 		//QUADTREE
 		timer.Restart();
 		environment->tree->BuildRoot();
 		environment->workerThreads.JoinWorkerThreads();
 		timer.Stop();
-		environment->stats.puQuadTreeUpdateTotalLastSecond += timer.ElapsedMilliseconds();
+		environment->stats.puQuadTreeUpdateTotalLastSecond += timer.ElapsedMicroseconds();
 
 		//PARTICLE COLLISIONS
 		timer.Restart();
@@ -164,7 +164,7 @@ void PhysicsEngine::LeadThreadRun() {
 			environment->workerThreads.AddWork([=] { QuadTreeParticleCollisions(&environment->quads, quadSection.lower, quadSection.upper); });
 		environment->workerThreads.JoinWorkerThreads();
 		timer.Stop();
-		environment->stats.puCollisionUpdateTotalLastSecond += timer.ElapsedMilliseconds();
+		environment->stats.puCollisionUpdateTotalLastSecond += timer.ElapsedMicroseconds();
 
 		//UPDATES
 		timer.Restart();
@@ -172,7 +172,7 @@ void PhysicsEngine::LeadThreadRun() {
 			environment->workerThreads.AddWork([=] { UpdateParticles(particleSection.lower, particleSection.upper, deltaTime); });
 		environment->workerThreads.JoinWorkerThreads();
 		timer.Stop();
-		environment->stats.puPositionUpdatesTotalLastSecond += timer.ElapsedMilliseconds();
+		environment->stats.puPositionUpdatesTotalLastSecond += timer.ElapsedMicroseconds();
 
 		++environment->stats.physicsUpdateTotalLastSecond;
 	}
